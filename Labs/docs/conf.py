3787548479e9674a1bea5d8d0365dd740d5dc122
Alexandre Lecomte <61118120+tribulle@users.noreply.github.com>
2024-09-25 19:14:52 +0200

    folder modifications
    modification des dossiers

--- Labs/docs/conf.py
+++ Labs/docs/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0,os.path.abspath(".."))
+sys.path.insert(0,os.path.abspath("../Labs"))
 
 project = 'Python Labs'
 copyright = '2024, LECOMTE, TRAORE'
@@ -19,7 +19,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc"]
+extensions = ["nbsphinx","sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
